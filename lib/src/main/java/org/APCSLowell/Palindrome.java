19cd758d24352d8247147c42e229473cf25d4dfe
jaketu-maker <jake.tu35@gmail.com>
2026-01-27 17:16:53 +0000

    1

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,20 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
+        String t = new String("");
+        for(int i = s.length()-1; i >= 0;i--){
+            t = t + s.charAt(i);
+        }
+        if(t.equals(s)){
+            return true;
+        }
         return false;
     }
     public String reverse(String s) {
         String n = new String();
-        // your code here
+        for(int i = s.length()-1; i >= 0;i--){
+            n = n + s.charAt(i);
+        }
         return n;
     }
 }
