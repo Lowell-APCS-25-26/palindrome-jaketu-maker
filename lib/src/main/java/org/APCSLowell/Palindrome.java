928d7f8f803edc6a6c35d1f82c6bea04224c3b07
jaketu-maker <jake.tu35@gmail.com>
2026-01-29 02:34:35 +0000

    8

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -1,21 +1,22 @@
 package org.APCSLowell;
 
 public class Palindrome {
+
     public boolean palindrome(String s) {
         String l = s.toLowerCase();
         String m = new String ("");
         String t = new String("");
         for(int i = l.length()-1; i >= 0;i--){
-            if(l.charAt(i) != ' ' || Character.isLetter(l.charAt(i))){
+            if(l.charAt(i) != ' ' && Character.isLetter(l.charAt(i))){
                 t = t + l.charAt(i);
             }
         }
         for(int i = 0; i < l.length();i++){
-            if(l.charAt(i) != ' ' || Character.isLetter(l.charAt(i))){
+            if(l.charAt(i) != ' ' && Character.isLetter(l.charAt(i))){
                 m = m + l.charAt(i);
             }
         }
-        if(t.equals(l)){
+        if(t.equals(m)){
             return true;
         }
         return false;
